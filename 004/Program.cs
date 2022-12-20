12a9e4f54575d039429032c79f6d3f31090f349a
Vladimir <dr.frisbi@gmail.com>
2022-12-21 00:06:25 +0700

    добавили код для решения задачи 4

--- 004/Program.cs
+++ 004/Program.cs
@@ -1,1 +1,27 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите третье число: ");
+int numC = Convert.ToInt32(Console.ReadLine());
+
+int max = numA;
+
+if (numA > max)
+{
+    max = numA;
+}
+
+if (numB > max)
+{
+    max = numB;
+}
+
+if (numC > max)
+{
+    max = numC;    
+}
+
+Console.Write($"Число {max} является максимальным");

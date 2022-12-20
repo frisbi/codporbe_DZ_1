6d489c8b34a284c22a349be94f84ea2133b8061e
Vladimir <dr.frisbi@gmail.com>
2022-12-20 23:44:54 +0700

    написали код для решения

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,1 +1,14 @@
-﻿
+﻿Console.Write("Введите первое число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+if (numA > numB)
+{
+    Console.Write($"Число {numA} является максимальным, а число {numB} минимальным");
+}
+else
+{
+    Console.Write($"Число {numB} является максимальным, а число {numA} минимальным");
+}

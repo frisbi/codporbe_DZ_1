24cfb1039876cd47ba6d8bce26eb18ad05e57fa0
Vladimir <dr.frisbi@gmail.com>
2022-12-21 00:48:20 +0700

    добавили код для решения задачи 8

---
+++ 008/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Введите число больше 1, до которого включительно ведем перечисление четных чисел: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+
+if (numA < 1)
+{
+  Console.WriteLine($"Введено неверное число");  
+}
+
+else
+{
+
+    while (count <= numA)
+    {
+        if(count % 2 == 0)
+
+        Console.WriteLine($"Четные числа до {numA} включительно: {count}");
+        count++;
+    }
+}

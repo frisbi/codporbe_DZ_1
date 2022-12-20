9f244d1780328b0e53fa2c34f0009fc1f8f0a0c7
Vladimir <dr.frisbi@gmail.com>
2022-12-21 00:19:34 +0700

    добавили код для решения задачи 6

---
+++ 006/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число: ");
+int numA = Convert.ToInt32(Console.ReadLine());
+
+if (numA % 2 == 0 )
+{
+    Console.Write($"Число {numA} является четным числом");
+}
+else
+{
+    Console.Write($"Число {numA} является не четным числом");
+}
